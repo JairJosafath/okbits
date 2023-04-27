dd8d6bc5a4373ef188d8ab99970e70e2949e917f
HungryCompiler <58106977+JairJosafath@users.noreply.github.com>
2023-04-26 22:15:28 -0300

    dropzone for file

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,3 +1,51 @@
+"use client";
+
+import { DragEvent } from "react";
+
 export default function Main() {
-  return <div className="flex-1  bg-blue-200">Main</div>;
+  function getData(event: DragEvent<HTMLDivElement>) {
+    const dt = event.dataTransfer;
+    const files = dt.files;
+    console.log(files[0].name);
+  }
+  return (
+    <div className="flex-1  bg-blue-200">
+      <div
+        className="
+      h-52
+      bg-green-400
+      mx-7
+      my-10
+      border-2
+      border-gray-500
+      flex
+      justify-center
+      items-center
+      hover:bg-slate-700
+      hover:cursor-pointer
+      rounded-lg
+      border-dashed
+      hover:text-white
+      "
+        onDragEnter={(e) => {
+          e.preventDefault();
+        }}
+        onDragOver={(e) => {
+          e.preventDefault();
+        }}
+        onDragEnd={(e) => {
+          e.preventDefault();
+        }}
+        onDragLeave={(e) => {
+          e.preventDefault();
+        }}
+        onDrop={(e) => {
+          e.preventDefault();
+          getData(e);
+        }}
+      >
+        <p>Drop Your File Here</p>
+      </div>
+    </div>
+  );
 }
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -6,7 +6,7 @@ export default function Sidebar() {
       </div>
       <div>
         {["file1", "file2", "file3"].map((file) => (
-          <div className=" m-1 rounded-md px-2 hover:bg-red-100 hover:cursor-pointer">
+          <div className="m-1 rounded-md px-2 hover:bg-red-100 hover:cursor-pointer">
             {file}
           </div>
         ))}
