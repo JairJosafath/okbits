1c5880a60e0e609101c2a315620bc5ebeb2f6887
HungryCompiler <58106977+JairJosafath@users.noreply.github.com>
2023-05-03 17:21:47 -0300

    changing fetch to axios, hopefully works better with tanstack

---
+++ src/app/api/file.ts
@@ -0,0 +1,23 @@
+export const API = {
+  file: {
+    add: async () => {},
+    getOne: async () => {},
+    getMulti: async () => {},
+    update: async () => {},
+    delete: async () => {},
+  },
+  user: {
+    add: async () => {},
+    getOne: async () => {},
+    getMulti: async () => {},
+    update: async () => {},
+    delete: async () => {},
+  },
+  profile: {
+    add: async () => {},
+    getOne: async () => {},
+    getMulti: async () => {},
+    update: async () => {},
+    delete: async () => {},
+  },
+};
